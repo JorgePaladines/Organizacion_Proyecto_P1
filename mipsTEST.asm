9b2e5587c58c11815b1e378fee1669f034f34e90
Jorge Paladines <joedpabo@gmail.com>
2018-11-11 14:50:31 -0600

    avance de mips

--- mipsTEST.asm
+++ mipsTEST.asm
@@ -1,25 +1,61 @@
-	.data
-opcion:	.word	0
-saldo:	.word	0
-flag:	.word	1
-mensajeInicial:	.asciiz "Ingrese su opcion: \n"
-op1:	.asciiz	"1-Compra\n"
-op2:	.asciiz	"2-Retiro\n"
-op3:	.asciiz	"3-Depósito\n"
-op4:	.asciiz	"4-Verificar saldo disponible\n"
-op5:	.asciiz	"5-Salir\n"
+			.data
+opcion:			.word	'a'
+cantidades_productos:	.space	400 #Arreglo de 100 ints
+precios_producto: 	.double	0.00:100 #Arreglo de 100 doubles con ceros
 
-	.text
-main:
-	#Inicialización de las variables
-	li	$t0, 1
-	sw	$t0, flag	#guardar flag = 1
-	li	$t0, 0
-	sw	$t0, saldo	#guardar saldo = 0
+mensajeInicial1:	.asciiz "******************BIENVENIDO AL SUPERMERCADO KOALA ELECTRONICO******************\n"
+mensajeInicial2:	.asciiz "Opciones: \n"
+op1:			.asciiz	"1. Ingresar producto al carrito de compras\n"
+op2:			.asciiz	"2. Eliminar producto del carrito de compras\n"
+op3:			.asciiz	"3. Calcular total y aplicar descuento por afiliacion\n"
+op4:			.asciiz	"4. Ingresar forma de pago & finalizar la compra actual\n"
+op5:			.asciiz	"5. Cerrar programa\n"
 
-	#Impresión de opciones
+			.text
+
+
+_start:	
+	#Imprimir el menú
+	jal printMenu
+	
+	#input de la opción
+	li	$v0, 5
+	syscall
+	#guardar la opcion
+	sb	$v0, opcion	#guardar la opcion escogida
+	lb	$t0, opcion	#colocarla en variable temporal para su uso
+	
+	jal ingresarProductoCarrito
+	
+	
+	#Prueba de carga de datos
+	li	$v0, 1
+	la	$t0, cantidades_productos
+	lw	$a0, 0($t0)
+	syscall
+	lw	$a0, 4($t0)
+	syscall
+	
+	li	$v0, 3
+	l.d	$f0, precios_producto
+	add.d	$f12, $f0, $f10
+	syscall
+	l.d	$f0, precios_producto+8
+	add.d	$f12, $f0, $f10
+	syscall
+
+	
+	li	$v0, 10
+	syscall
+
+
+#Impresión de opciones
+printMenu:
 	li	$v0, 4
-	la	$a0, mensajeInicial
+	la	$a0, mensajeInicial1
+	syscall
+	li	$v0, 4
+	la	$a0, mensajeInicial2
 	syscall
 	la	$a0, op1
 	syscall
@@ -31,21 +67,33 @@ main:
 	syscall
 	la	$a0, op5
 	syscall
+
+	jr $ra
 	
-	#input de la opción
-	li	$v0, 5
+#Opción 1: Ingresar producto
+ingresarProductoCarrito:
+	addi $sp, $sp, -20
+	sw $t3, 16($sp)
+	sw $t4, 12($sp)
+	sw $t2, 8($sp)
+	s.d $f0, 0($sp)
+	
+	la $t3, cantidades_productos
+	la $t4, precios_producto
+
+	li $t2, 10
+	sw $t2, 0($t3)
+	li $t2, 20
+	sw $t2, 4($t3)
+	
+	li $v0, 7
 	syscall
-	#guardar la opcion
-	sb	$v0, opcion	#guardar la opcion escogida
-	lb	$t0, opcion	#colocarla en variable temporal para su uso
-	li	$v0, 1
-	move	$a0, $t0
+	s.d $f0, ($t4)
+	
+	li $v0, 7
 	syscall
-
-
-
+	addi $t4, $t4, 8
+	s.d $f0, ($t4)
 
 	
-	li	$v0, 10
-	syscall
-	
+	jr $ra
