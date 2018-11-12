4f5a9190f1fd248d4b2707434c30bff9a4e07f2c
Jorge Paladines <joedpabo@gmail.com>
2018-11-11 22:12:33 -0600

    Separa la funcion ingresarProductos e inicializada la funcion calcularTotal

--- mipsTEST.asm
+++ mipsTEST.asm
@@ -1,7 +1,10 @@
-			.data
-opcion:			.word	'a'
+.data
 cantidades_productos:	.space	400 #Arreglo de 100 ints
-precios_producto: 	.double	0.00:100 #Arreglo de 100 doubles con ceros
+precios_productos: 	.double	0.00:100 #Arreglo de 100 doubles con ceros
+DESCUENTO_AFI:		.double 0.95
+IVA:			.double	1.12
+total_precio:		.double	0.00
+total_productos:	.word	0
 
 mensajeInicial1:	.asciiz "******************BIENVENIDO AL SUPERMERCADO KOALA ELECTRONICO******************\n"
 mensajeInicial2:	.asciiz "Opciones: \n"
@@ -11,10 +14,13 @@ op3:			.asciiz	"3. Calcular total y aplicar descuento por afiliacion\n"
 op4:			.asciiz	"4. Ingresar forma de pago & finalizar la compra actual\n"
 op5:			.asciiz	"5. Cerrar programa\n"
 espacio:		.asciiz "\n"
+ingresoDeProductos:	.asciiz ">>>>>>>>> INGRESO DE PRODUCTOS <<<<<<<<<\n"
+ingreseNumeroProductos:	.asciiz "Ingrese el numero de productos de la compra: "
+str2:			.asciiz	"Cantidad no válida\n"
+strcalcularTotal:	.asciiz ">>>>>>>>> CALCULAR TOTAL <<<<<<<<<\n"
 
-			.text
 
-
+.text
 _start:	
 	#Imprimir el menú
 	jal printMenu
@@ -23,42 +29,76 @@ _start:
 	li	$v0, 5
 	syscall
 	#guardar la opcion
-	sb	$v0, opcion	#guardar la opcion escogida
-	lb	$t0, opcion	#colocarla en variable temporal para su uso
+	move	$t0, $v0	#guardar la opcion escogida
 	li $v0, 4
 	la $a0, espacio
 	syscall
 	
-	jal ingresarProductoCarrito
+	beq $t0, 1, OP1
+	beq $t0, 2, OP2
+	beq $t0, 3, OP3
+	beq $t0, 4, OP4
+	beq $t0, 5, OP5
+	
+	OP1:
+		li $v0, 4
+		la $a0, ingresoDeProductos
+		syscall
+		la $a0, ingreseNumeroProductos
+		syscall
+		
+		li $v0, 5 #scanf ("%d", &total_productos);
+		syscall
+		
+		add $t0, $v0, $0 #que el número ingresado por pantalla se guarde en $t0
+		li $v0, 4
+		la $a0, espacio
+		syscall
+		
+		#validaicón de cantidad de productos
+		while:
+			slti $t1, $t0, 1
+			beq $t1, $zero, noWhile
+			li $v0, 4
+			la $a0, str2 #printf("Cantidad no válida\n");
+			syscall
+			li $v0, 4
+			la $a0, espacio
+			syscall
+			li $v0, 5 #scanf ("%d", &total_productos);
+			syscall
+			add $t0, $v0, $0 #que el número ingresado por pantalla se guarde en $t0
+			li $v0, 4
+			la $a0, espacio
+			syscall
+			j while
+		
+		noWhile:
+			sw $t0, total_productos
+			la $a0, cantidades_productos
+			la $a1, precios_productos
+			addi $a2, $t0, 0
+			jal ingresarProductoCarrito
+
+	OP2:
+	
+	OP3:
+		li $v0, 4
+		la $a0, strcalcularTotal
+		syscall
+		
+		la $a0, cantidades_productos
+		la $a1, precios_productos
+		sw $t0, total_productos
+		addi $a2, $t0, 0
+		jal calcularTotal
+		s.d $f0, total_precio
+	
+	OP4:
 	
 	
-	#Prueba de carga de datos
-	li	$v0, 1
-	la	$t0, cantidades_productos
-	lw	$a0, 0($t0)
-	syscall
-	li	$v0, 3
-	l.d	$f0, precios_producto
-	add.d	$f12, $f0, $f10
-	syscall
+	OP5:
 	
-	li	$v0, 1
-	la	$t0, cantidades_productos
-	lw	$a0, 4($t0)
-	syscall
-	li	$v0, 3
-	l.d	$f0, precios_producto+8
-	add.d	$f12, $f0, $f10
-	syscall
-	
-	li	$v0, 1
-	la	$t0, cantidades_productos
-	lw	$a0, 8($t0)
-	syscall
-	li	$v0, 3
-	l.d	$f0, precios_producto+16
-	add.d	$f12, $f0, $f10
-	syscall
 	
 
 	
@@ -90,8 +130,6 @@ printMenu:
 #Opción 1: Ingresar producto
 ingresarProductoCarrito:
 	.data
-	str1:	.asciiz	"Ingrese el numero de productos de la compra: "
-	str2:	.asciiz	"Cantidad no válida\n"
 	str3:	.asciiz	"Producto Nº "
 	str4: 	.asciiz "Cantidad: "
 	str5:	.asciiz	"Precio: "
@@ -104,51 +142,21 @@ ingresarProductoCarrito:
 	s.d $f0, 24($sp) #donde se guarda el input de cada precio
 	sw $t6, 20($sp) #ctd
 	sw $t5, 16($sp) #index
-	sw $t0, 12($sp) #cantidad_productos
+	sw $s3, 12($sp) #total_productos
 	sw $s1, 8($sp) #cantidades_productos
 	sw $s2, 4($sp) #precios_producto
 	sw $t2, 0($sp) #i del for
 	
 	
 	#Obtener los arreglos para usarlos dentro de la función
-	la $s1, cantidades_productos
-	la $s2, precios_producto
+	la $s1, 0($a0) #cantidad
+	la $s2, 0($a1) #precio
+	la $s3, 0($a2) #size
 	
-	li $t0, 0 #int cantidad_productos = 0
-	li $v0, 4
-	la $a0, str1 #printf("Ingrese el numero de productos de la compra: ");
-	syscall
 	
-	li $v0, 5 #scanf ("%d", &cantidad_productos);
-	syscall
-	add $t0, $v0, $0 #que el número ingresado por pantalla se guarde en $t0
-	li $v0, 4
-	la $a0, espacio
-	syscall
-	
-	#validaicón de cantidad de productos
-	while:
-		slti $t1, $t0, 1
-		beq $t1, $zero, noWhile
-		li $v0, 4
-		la $a0, str2 #printf("Cantidad no válida\n");
-		syscall
-		li $v0, 4
-		la $a0, espacio
-		syscall
-		li $v0, 5 #scanf ("%d", &cantidad_productos);
-		syscall
-		add $t0, $v0, $0 #que el número ingresado por pantalla se guarde en $t0
-		li $v0, 4
-		la $a0, espacio
-		syscall
-		j while
-	
-	noWhile:
 	li $t2, 0 #int i = 0
-	
 	For:
-		slt $t1, $t2, $t0 # i < cantidad_productos
+		slt $t1, $t2, $s3 # i < cantidad_productos
 		beq $t1, $zero, finFor
 		addi $t5, $t2, 1 #int index = i + 1;
 		li $t6, 0 #int ctd = 0;
@@ -194,12 +202,22 @@ ingresarProductoCarrito:
 		
 	finFor:
 	#Restaurar registros
-	sw $t0, 0($sp)
-	lw $t3, 4($sp)
-	lw $t4, 8($sp)
-	lw $t2, 12($sp)
-	lw $t3, 16($sp) #index
-	lw $t4, 20($sp) #ctd
+	lw $t2, 0($sp)
+	lw $s2, 4($sp)
+	lw $s1, 8($sp)
+	lw $s3, 12($sp)
+	lw $t5, 16($sp) #index
+	lw $t6, 20($sp) #ctd
 	l.d $f0, 24($sp)
 	addi $sp, $sp, 36
 	jr $ra
+
+
+#Opción 3: Calcular Total
+calcularTotal:
+	
+
+	la $s1, 0($a0) #cantidad
+	la $s2, 0($a1) #precio
+	la $s3, 0($a2) #size
+	
