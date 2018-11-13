947a6fd935dfe9b751ab667e37e68d609838da35
Jorge Paladines <joedpabo@gmail.com>
2018-11-12 22:17:05 -0600

    funcion calcular haciendose

--- mipsTEST.asm
+++ mipsTEST.asm
@@ -1,5 +1,5 @@
 .data
-cantidades_productos:	.space	400 #Arreglo de 100 ints
+cantidades_productos:	.word	0:100 #Arreglo de 100 ints
 precios_productos: 	.double	0.00:100 #Arreglo de 100 doubles con ceros
 DESCUENTO_AFI:		.double 0.95
 IVA:			.double	1.12
@@ -192,7 +192,7 @@ ingresarProductoCarrito:
 		
 		sll $t4, $t2, 3
 		add $t4, $t4, $s2 #precio[i]
-		s.d $f0, 0($t4)
+		s.d $f0, 0($t4) #precio[i] = pr
 		
 		addi $t2, $t2, 1 #i++
 		
@@ -203,23 +203,150 @@ ingresarProductoCarrito:
 		j For
 		
 	finFor:
-	#Restaurar registros
-	lw $t2, 0($sp)
-	lw $s2, 4($sp)
-	lw $s1, 8($sp)
-	lw $s3, 12($sp)
-	lw $t5, 16($sp) #index
-	lw $t6, 20($sp) #ctd
-	l.d $f0, 24($sp)
-	addi $sp, $sp, 36
-	jr $ra
+		#Restaurar registros
+		lw $t2, 0($sp)
+		lw $s2, 4($sp)
+		lw $s1, 8($sp)
+		lw $s3, 12($sp)
+		lw $t5, 16($sp) #index
+		lw $t6, 20($sp) #ctd
+		l.d $f0, 24($sp)
+		addi $sp, $sp, 36
+		jr $ra
 
 
 #Opción 3: Calcular Total
 calcularTotal:
-	
-
-	la $s1, 0($a0) #cantidad
-	la $s2, 0($a1) #precio
-	la $s3, 0($a2) #size
-	
+	.data
+		str1cal:	.asciiz	"¿El cliente esta afiliado a SUPERMERCADOS KOALA? \n [1] Si [Cualquier Tecla] No \n"
+		str2cal:	.asciiz	">>Usted ha presionado "
+		str3cal:	.asciiz	"-------------Detalle de la compra--------------\n"
+		str4cal:	.asciiz	"Producto Nº "
+		str5cal:	.asciiz	"	|Cantidad: "
+		str6cal:	.asciiz	" 	|Precio: $"
+		str7cal:	.asciiz	" 	|Precio Final: $"
+		str8cal:	.asciiz "Total: $"
+		str9cal:	.asciiz "Total con descuento: $"
+		str10cal:	.asciiz "Usted ahorró "
+		str11cal:	.asciiz " en esta compra por ser afiliado\n"
+		afi:		.space	4
+		opAfi:		.byte	'1'
+	.text
+		
+		la $s1, 0($a0) #cantidad
+		l.d $f0, 0($a1) #precio
+		la $s3, 0($a2) #size
+		
+		li $v0, 4
+		la $a0, str1cal #"¿El cliente esta afiliado a SUPERMERCADOS KOALA? \n [1] Si [Cualquier Tecla] No \n"
+		syscall
+		
+		li $v0, 8 #scanf (" %c", &afi);
+		la $a0, afi
+		li $a1, 4
+		syscall
+		
+		li $v0, 4
+		la $a0, str2cal #">>Usted ha presionado "
+		syscall
+		lb $a0, afi
+		syscall
+		
+		li $v0, 4
+		la $a0, espacio
+		syscall
+		la $a0, str3cal #"-------------Detalle de la compra--------------\n"
+		syscall
+		
+		li $t2, 0 #int i = 0
+		li $v0, 4
+		la $a0, afi
+		syscall
+		calFor:
+			slt $t1, $t2, $s3 # i < size
+			beq $t1, $zero, calfinFor
+			
+			sll $t0, $t2, 2 # i * 4
+			sll $t1, $t2, 3 # i * 4
+			add $t0, $t0, $s1 #cantidad[i]
+			mtc1.d $t0, $f2
+  			cvt.d.w $f2, $f2 #convertir cantidad[i] a un double
+			add.d $f4, $f4, $f0 #precio[i]
+			
+			#$f2 = cantidad[i], $f4 = precio[i], IVA = iva = 1.12
+			
+			mul.d $f6, $f2, $f4 #precio[i]*cantidad[i]
+			l.d $f8, IVA 
+			mul.d $f6, $f6, $f8 #precio[i]*cantidad[i]*IVA
+			add.d $f14, $f24, $f6 #total += precio_final;
+			
+			#$t0 = cantidad[i] como entero
+			#$f4 = precio[i]
+			#$f6 = precio_final
+			#$f14 = total
+			
+			#"Producto Nº "
+			li $v0, 4
+			la $a0, str4cal
+			syscall
+			#número del producto
+			li $v0, 1
+			addi $a0, $t2, 1
+			syscall
+			#"	|Cantidad: "
+			li $v0, 4
+			la $a0, str5cal
+			syscall
+			#cantidad de productos
+			li $v0, 1
+			lw $a0, 0($t0)
+			syscall
+			#" 	|Precio: $"
+			li $v0, 4
+			la $a0, str6cal 
+			syscall
+			#precio del producto
+			li $v0, 3
+			add.d $f12, $f4, $f24
+			syscall
+			#" 	|Precio Final: $"
+			li $v0, 4
+			la $a0, str7cal
+			syscall
+			#precio_final
+			li $v0, 3
+			add.d $f12, $f14, $f24
+			syscall
+			
+			li $v0, 4
+			la $a0, espacio
+			syscall
+			
+			addi $t2, $t2, 1 #i++
+			j calFor
+			
+		calfinFor:
+		li $v0, 4
+		la $a0, str8cal #"Total: $"
+		syscall
+		li $v0, 3
+		add.d $f12, $f14, $f24
+		syscall #printf("Total: $%lf\n", total);
+		li $v0, 4
+		la $a0, espacio
+		syscall
+		
+		lw $s4, afi
+		lw $s5, opAfi
+		bne $s4, $s5, afiNoIgual1
+		
+		li $v0, 4
+		lb $a0, afi
+		syscall
+		la $a0, opAfi
+		syscall
+		
+		afiNoIgual1:
+			
+		
+		jr $ra
